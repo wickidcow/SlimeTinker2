1fd8216689aa3a84750ad020f73c53e914adc03d
wickidcow <jaywago@gmail.com>
2026-07-07 21:56:40 -0400

    Add files via upload

--- src/main/java/net/guizhanss/minecraft/slimetinker/utils/IdMap.java
+++ src/main/java/net/guizhanss/minecraft/slimetinker/utils/IdMap.java
@@ -6,130 +6,137 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 包含大部分 {@link Ids} 对应的中文
+ * {@link Ids} 
  */
 public class IdMap {
 
-    private IdMap() {
-        throw new IllegalStateException("Utility class");
-    }
+ private IdMap() {
+ throw new IllegalStateException("Utility class");
+ }
 
-    private static final Map<String, String> idMap = new HashMap<>();
+ private static final Map<String, String> idMap = new HashMap<>();
 
-    static {
-        // Vanilla + Core Sf
-        idMap.put(Ids.IRON, "铁");
-        idMap.put(Ids.GOLD, "金");
-        idMap.put(Ids.COPPER, "铜");
-        idMap.put(Ids.SILVER, "银");
-        idMap.put(Ids.TIN, "锡");
-        idMap.put(Ids.MAGNESIUM, "镁");
-        idMap.put(Ids.ALUMINUM, "铝");
-        idMap.put(Ids.LEAD, "铅");
-        idMap.put(Ids.ZINC, "锌");
-        idMap.put(Ids.COAL, "煤");
-        idMap.put(Ids.STEEL, "钢");
-        idMap.put(Ids.DAMASCUS_STEEL, "大马士革钢");
-        idMap.put(Ids.DURALUMIN, "硬铝");
-        idMap.put(Ids.BRONZE, "青铜");
-        idMap.put(Ids.ALUMINUM_BRONZE, "铝青铜");
-        idMap.put(Ids.HARDENED_METAL, "硬化金属");
-        idMap.put(Ids.CORINTHIAN_BRONZE, "科林斯青铜");
-        idMap.put(Ids.SOLDER, "焊锡");
-        idMap.put(Ids.BILLON, "银铜合金");
-        idMap.put(Ids.BRASS, "黄铜");
-        idMap.put(Ids.ALUMINUM_BRASS, "铝黄铜");
-        idMap.put(Ids.NICKEL, "镍");
-        idMap.put(Ids.COBALT, "钴");
-        idMap.put(Ids.REINFORCED_ALLOY, "强化合金");
-        idMap.put(Ids.STRING, "线");
-        idMap.put(Ids.VINE, "藤蔓");
-        idMap.put(Ids.CRIMSON_ROOTS, "绯红菌索");
-        idMap.put(Ids.WARPED_ROOTS, "诡异菌索");
-        idMap.put(Ids.WEEPING_VINES, "垂泪藤");
-        idMap.put(Ids.TWISTING_VINES, "缠怨藤");
-        idMap.put(Ids.SLIME, "粘液");
-        idMap.put(Ids.LEATHER, "皮革");
-        idMap.put(Ids.DIAMOND, "钻石");
-        idMap.put(Ids.REDSTONE, "红石");
-        idMap.put(Ids.REDSTONE_ALLOY, "红石合金");
-        idMap.put(Ids.SILICON, "硅");
-        idMap.put(Ids.FERROSILICON, "硅铁");
-        idMap.put(Ids.BOOMERITE, "Boomer合金");
-        idMap.put(Ids.SEFIRITE, "Sefi合金");
-        idMap.put(Ids.LIQUID_CHRISTMAS, "圣诞礼物");
-        idMap.put(Ids.CRINGLEIUM, "圣诞合金");
-        idMap.put(Ids.FLOWING_FONDNESS, "喜爱");
-        idMap.put(Ids.DETAILED_DEVOTION, "深爱");
-        idMap.put(Ids.PURIFIED_PASSION, "热情");
-        idMap.put(Ids.LIQUID_LOVE, "爱");
-        idMap.put(Ids.NICEINIUM, "Nice");
-        idMap.put(Ids.DRACONIC, "龙息");
-        idMap.put(Ids.REINFORCED_DRACONIUM, "强化龙");
+ static {
+ // Vanilla + Core Sf
+ idMap.put(Ids.IRON, "Iron");
+ idMap.put(Ids.GOLD, "Gold");
+ idMap.put(Ids.COPPER, "Copper");
+ idMap.put(Ids.SILVER, "Silver");
+ idMap.put(Ids.TIN, "Tin");
+ idMap.put(Ids.MAGNESIUM, "Magnesium");
+ idMap.put(Ids.ALUMINUM, "Aluminum");
+ idMap.put(Ids.LEAD, "Lead");
+ idMap.put(Ids.ZINC, "Zinc");
+ idMap.put(Ids.COAL, "Coal");
+ idMap.put(Ids.STEEL, "Steel");
+ idMap.put(Ids.DAMASCUS_STEEL, "Damascus Steel");
+ idMap.put(Ids.DURALUMIN, "Duralumin");
+ idMap.put(Ids.BRONZE, "Bronze");
+ idMap.put(Ids.ALUMINUM_BRONZE, "Aluminum Bronze");
+ idMap.put(Ids.HARDENED_METAL, "Hardened Metal");
+ idMap.put(Ids.CORINTHIAN_BRONZE, "Corinthian Bronze");
+ idMap.put(Ids.SOLDER, "Solder");
+ idMap.put(Ids.BILLON, "Billon");
+ idMap.put(Ids.BRASS, "Brass");
+ idMap.put(Ids.ALUMINUM_BRASS, "Aluminum Brass");
+ idMap.put(Ids.NICKEL, "Nickel");
+ idMap.put(Ids.COBALT, "Cobalt");
+ idMap.put(Ids.REINFORCED_ALLOY, "Reinforced Alloy");
+ idMap.put(Ids.STRING, "String");
+ idMap.put(Ids.VINE, "Vine");
+ idMap.put(Ids.CRIMSON_ROOTS, "Crimson Roots");
+ idMap.put(Ids.WARPED_ROOTS, "Warped Roots");
+ idMap.put(Ids.WEEPING_VINES, "Weeping Vines");
+ idMap.put(Ids.TWISTING_VINES, "Twisting Vines");
+ idMap.put(Ids.SLIME, "Slime");
+ idMap.put(Ids.LEATHER, "Leather");
+ idMap.put(Ids.DIAMOND, "Diamond");
+ idMap.put(Ids.REDSTONE, "Redstone");
+ idMap.put(Ids.REDSTONE_ALLOY, "Redstone Alloy");
+ idMap.put(Ids.SILICON, "Silicon");
+ idMap.put(Ids.FERROSILICON, "Ferrosilicon");
+ idMap.put(Ids.BOOMERITE, "Boomerite");
+ idMap.put(Ids.SEFIRITE, "Sefirite");
+ idMap.put(Ids.LIQUID_CHRISTMAS, "Christmas Present");
+ idMap.put(Ids.CRINGLEIUM, "Cringleium");
+ idMap.put(Ids.FLOWING_FONDNESS, "FondnessLove");
+ idMap.put(Ids.DETAILED_DEVOTION, "DevotionLove");
+ idMap.put(Ids.PURIFIED_PASSION, "Passion");
+ idMap.put(Ids.LIQUID_LOVE, "Love");
+ idMap.put(Ids.NICEINIUM, "Nice");
+ idMap.put(Ids.DRACONIC, "");
+ idMap.put(Ids.REINFORCED_DRACONIUM, "Reinforced Draconium");
 
-        // Mods
-        idMap.put("QUARTZ", "石英");
-        idMap.put("LAPIS_LAZULI", "青金石");
-        idMap.put("MOD_PLATE", "强化合金板");
-        idMap.put("EMERALD", "绿宝石");
+ // Mods
+ idMap.put("QUARTZ", "Quartz");
+ idMap.put("LAPIS_LAZULI", "Lapis Lazuli");
+ idMap.put("MOD_PLATE", "Reinforced AlloyPlate");
+ idMap.put("EMERALD", "Emerald");
 
-        // Infinity Expansion (SlimefunGuguProject)
-        idMap.put(Ids.IE_VOID, "虚空");
-        idMap.put(Ids.INFINITY, "无尽");
-        idMap.put(Ids.IE_FORTUNE_SINGULARITY, "时运奇点");
-        idMap.put(Ids.IE_MAGIC_SINGULARITY, "魔法奇点");
-        idMap.put(Ids.IE_EARTH_SINGULARITY, "地球奇点");
-        idMap.put(Ids.IE_METAL_SINGULARITY, "金属奇点");
-        idMap.put(Ids.IE_MAGSTEEL, "磁钢");
-        idMap.put(Ids.IE_TITANIUM, "钛");
-        idMap.put(Ids.IE_MYTHRIL, "秘银");
-        idMap.put(Ids.IE_ADAMANTITE, "精金");
-        idMap.put(Ids.IE_MAGNONIUM, "磁金");
-        idMap.put(Ids.IE_COPPER_SINGULARITY, "铜奇点");
-        idMap.put(Ids.IE_ZINC_SINGULARITY, "锌奇点");
-        idMap.put(Ids.IE_TIN_SINGULARITY, "锡奇点");
-        idMap.put(Ids.IE_ALUMINUM_SINGULARITY, "铝奇点");
-        idMap.put(Ids.IE_SILVER_SINGULARITY, "银奇点");
-        idMap.put(Ids.IE_MAGNESIUM_SINGULARITY, "镁奇点");
-        idMap.put(Ids.IE_LEAD_SINGULARITY, "铅奇点");
-        idMap.put(Ids.IE_GOLD_SINGULARITY, "金奇点");
-        idMap.put(Ids.IE_IRON_SINGULARITY, "铁奇点");
-        idMap.put(Ids.IE_DIAMOND_SINGULARITY, "钻石奇点");
-        idMap.put(Ids.IE_INFINITY_SINGULARITY, "无尽奇点");
+ // Infinity Expansion (SlimefunGuguProject)
+ idMap.put(Ids.VOID, "Void");
+ idMap.put(Ids.INFINITY, "Infinity");
+ idMap.put(Ids.FORTUNE_SINGULARITY, "Fortune Singularity");
+ idMap.put(Ids.MAGIC_SINGULARITY, "Magic Singularity");
+ idMap.put(Ids.EARTH_SINGULARITY, "Earth Singularity");
+ idMap.put(Ids.METAL_SINGULARITY, "Metal Singularity");
+ idMap.put(Ids.MAGSTEEL, "Magsteel");
+ idMap.put(Ids.TITANIUM, "Titanium");
+ idMap.put(Ids.MYTHRIL, "Mythril");
+ idMap.put(Ids.ADAMANTITE, "Adamantite");
+ idMap.put(Ids.MAGNONIUM, "Magnonium");
+ idMap.put(Ids.COPPER_SINGULARITY, "Copper Singularity");
+ idMap.put(Ids.ZINC_SINGULARITY, "Zinc Singularity");
+ idMap.put(Ids.TIN_SINGULARITY, "Tin Singularity");
+ idMap.put(Ids.ALUMINUM_SINGULARITY, "Aluminum Singularity");
+ idMap.put(Ids.SILVER_SINGULARITY, "Silver Singularity");
+ idMap.put(Ids.MAGNESIUM_SINGULARITY, "Magnesium Singularity");
+ idMap.put(Ids.LEAD_SINGULARITY, "Lead Singularity");
+ idMap.put(Ids.GOLD_SINGULARITY, "Gold Singularity");
+ idMap.put(Ids.IRON_SINGULARITY, "Iron Singularity");
+ idMap.put(Ids.DIAMOND_SINGULARITY, "Diamond Singularity");
+ idMap.put(Ids.INFINITY_SINGULARITY, "Infinity Singularity");
 
-        // Slimefun Warfare
-        idMap.put(Ids.SLIMESTEEL, "软钢");
-        idMap.put(Ids.REINFORCED_SLIMESTEEL, "强化软钢");
-        idMap.put(Ids.OSMIUM_SUPERALLOY, "锇高温合金");
-        idMap.put(Ids.OSMIUM, "锇");
-        idMap.put(Ids.SEGGANESSON, "塞格尼森");
-        idMap.put(Ids.UNPATENTABLIUM, "非专利品");
+ // Slimefun Warfare
+ idMap.put(Ids.SLIMESTEEL, "Slimesteel");
+ idMap.put(Ids.REINFORCED_SLIMESTEEL, "Reinforced Slimesteel");
+ idMap.put(Ids.OSMIUM_SUPERALLOY, "Osmium Superalloy");
+ idMap.put(Ids.OSMIUM, "Osmium");
+ idMap.put(Ids.SEGGANESSON, "");
+ idMap.put(Ids.UNPATENTABLIUM, "Unpatentablium");
 
-        // LiteXpansion
-        idMap.put(Ids.RUBBER, "橡胶");
-        idMap.put(Ids.REFINED_IRON, "精炼铁");
-        idMap.put(Ids.MIXED_METAL, "合金");
-        idMap.put(Ids.ADVANCED_ALLOY, "高级合金");
-        idMap.put(Ids.THORIUM, "钍");
-        idMap.put(Ids.MAG_THOR, "镁钍合金");
-        idMap.put(Ids.CARBON_MESH, "粗制碳板");
-        idMap.put(Ids.SCRAP, "废料");
-        idMap.put(Ids.IRIDIUM, "铱");
+ // DynaTech
+ idMap.put(Ids.STAINLESS_STEEL, "Stainless Steel");
+ idMap.put(Ids.VEX_GEM, "Vex Gem");
+ idMap.put(Ids.STARDUST, "Stardust");
+ idMap.put(Ids.GHOSTLY_ESSENCE, "Ghostly Essence");
+ idMap.put(Ids.TESSERACT, "Tesseracting Material");
 
-        // TranscEndence
-        idMap.put(Ids.DAXI_STRENGTH, "超能力核心(S)");
-        idMap.put(Ids.DAXI_ABSORPTION, "超能力核心(A)");
-        idMap.put(Ids.DAXI_FORTITUDE, "超能力核心(F)");
-        idMap.put(Ids.DAXI_SATURATION, "超能力核心(H)");
-        idMap.put(Ids.DAXI_REGENERATION, "超能力核心(R)");
+ // LiteXpansion
+ idMap.put(Ids.RUBBER, "Rubber");
+ idMap.put(Ids.REFINED_IRON, "Refined Iron");
+ idMap.put(Ids.MIXED_METAL, "Alloy");
+ idMap.put(Ids.ADVANCED_ALLOY, "Advanced Alloy");
+ idMap.put(Ids.THORIUM, "Thorium");
+ idMap.put(Ids.MAG_THOR, "Mag-Thor");
+ idMap.put(Ids.CARBON_MESH, "Carbon Mesh");
+ idMap.put(Ids.SCRAP, "Scrap");
+ idMap.put(Ids.IRIDIUM, "Iridium");
 
-        // Networks
-        idMap.put(Ids.MOLTEN_PRESENCE, "跨越维度的存在");
-        idMap.put(Ids.REMOTININIUM, "古代远程访问");
-        idMap.put(Ids.ULTIMANINIUM, "终极远程访问");
-    }
+ // TranscEndence
+ idMap.put(Ids.DAXI_STRENGTH, "Daxi Core(S)");
+ idMap.put(Ids.DAXI_ABSORPTION, "Daxi Core(A)");
+ idMap.put(Ids.DAXI_FORTITUDE, "Daxi Core(F)");
+ idMap.put(Ids.DAXI_SATURATION, "Daxi Core(H)");
+ idMap.put(Ids.DAXI_REGENERATION, "Daxi Core(R)");
 
-    public static Map<String, String> getMap() {
-        return idMap;
-    }
+ // Networks
+ idMap.put(Ids.MOLTEN_PRESENCE, "Molten Presence");
+ idMap.put(Ids.REMOTININIUM, "Ancient Remote Access");
+ idMap.put(Ids.ULTIMANINIUM, "Ultimate Remote Access");
+ }
+
+ public static Map<String, String> getMap() {
+ return idMap;
+ }
 }
--- src/main/java/net/guizhanss/minecraft/slimetinker/utils/LangUtils.java
+++ src/main/java/net/guizhanss/minecraft/slimetinker/utils/LangUtils.java
@@ -5,66 +5,66 @@ import io.github.sefiraat.slimetinker.utils.ThemeUtils;
 
 public final class LangUtils {
 
-    private LangUtils() {
-        throw new IllegalStateException("Utility class");
-    }
+ private LangUtils() {
+ throw new IllegalStateException("Utility class");
+ }
 
-    public static String getMaterialName(String type) {
-        String result = IdMap.getMap().get(type.toUpperCase());
-        return result != null ? result : ThemeUtils.toTitleCase(type);
-    }
+ public static String getMaterialName(String type) {
+ String result = IdMap.getMap().get(type.toUpperCase());
+ return result != null ? result : ThemeUtils.toTitleCase(type);
+ }
 
-    public static String getToolName(String type){
-        switch (type.toUpperCase()) {
-            case Ids.PICKAXE:
-                return "镐";
-            case Ids.SHOVEL:
-                return "铲";
-            case Ids.AXE:
-                return "斧";
-            case Ids.SWORD:
-                return "剑";
-            case Ids.HOE:
-                return "锄";
-            default:
-                return ThemeUtils.toTitleCase(type);
-        }
-    }
+ public static String getToolName(String type){
+ switch (type.toUpperCase()) {
+ case Ids.PICKAXE:
+ return "Pickaxe";
+ case Ids.SHOVEL:
+ return "Shovel";
+ case Ids.AXE:
+ return "Axe";
+ case Ids.SWORD:
+ return "Sword";
+ case Ids.HOE:
+ return "Hoe";
+ default:
+ return ThemeUtils.toTitleCase(type);
+ }
+ }
 
-    public static String getToolHead(String type){
-        switch (type.toUpperCase()) {
-            case Ids.PICKAXE:
-                return "镐头";
-            case Ids.SHOVEL:
-                return "铲头";
-            case Ids.AXE:
-                return "斧刃";
-            case Ids.SWORD:
-                return "剑刃";
-            case Ids.HOE:
-                return "锄头";
-            default:
-                return ThemeUtils.toTitleCase(type);
-        }
-    }
+ public static String getToolHead(String type){
+ switch (type.toUpperCase()) {
+ case Ids.PICKAXE:
+ return "PickaxeHead";
+ case Ids.SHOVEL:
+ return "ShovelHead";
+ case Ids.AXE:
+ return "AxeBlade";
+ case Ids.SWORD:
+ return "SwordBlade";
+ case Ids.HOE:
+ return "HoeHead";
+ default:
+ return ThemeUtils.toTitleCase(type);
+ }
+ }
 
-    public static String getArmorPart(String part) {
-        switch (part.toUpperCase()) {
-            case Ids.HELMET:
-                return "头盔";
-            case Ids.CHESTPLATE:
-                return "胸甲";
-            case Ids.LEGGINGS:
-                return "护腿";
-            case Ids.BOOTS:
-                return "靴子";
-            default:
-                return ThemeUtils.toTitleCase(part);
-        }
-    }
+ public static String getArmorPart(String part) {
+ switch (part.toUpperCase()) {
+ case Ids.HELMET:
+ return "Helmet";
+ case Ids.CHESTPLATE:
+ return "Chestplate";
+ case Ids.LEGGINGS:
+ return "Leggings";
+ case Ids.BOOTS:
+ return "Boots";
+ default:
+ return ThemeUtils.toTitleCase(part);
+ }
+ }
 
-    public static String getToolOrArmorName(String type){
-        String toolName = getToolName(type);
-        return toolName.equals(ThemeUtils.toTitleCase(type)) ? getArmorPart(type) : toolName;
-    }
+ public static String getToolOrArmorName(String type){
+ String toolName = getToolName(type);
+ return toolName.equals(ThemeUtils.toTitleCase(type)) ? getArmorPart(type) : toolName;
+ }
 }
